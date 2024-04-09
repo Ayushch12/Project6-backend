b5103ce5489697a4e8b26d23090b1cccc02e2c1d
Ayush chalisee <ayushchalise12@gmail.com>
2024-04-09 21:14:08 +0200

    Updated

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 
 const bcrypt= require('bcrypt');
 const jwt= require('jsonwebtoken');
-const User = require('../models/user') ;
+const User = require('../models/User') ;
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10).then(
@@ -27,7 +27,7 @@ exports.signup = (req, res, next) => {
     );
   };
 
- 
+
 
 
 
@@ -47,12 +47,12 @@ exports.login = (req, res, next) => {
               error: new Error('Incorrect password!')
             });
           }
-         
+
 const token= jwt.sign(
     { userId: user._id},
     'RANDOM_TOKEN_SECRET',
     { expiresIn : '24h' });
- 
+
           res.status(200).json({
             userId: user._id,
             token: token
